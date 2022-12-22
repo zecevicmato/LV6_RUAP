2c99053dbc46423e965a9cc000b26d9b95a61be0
Tin Jancic <tinjancic70@gmail.com>
2022-12-22 16:08:03 +0100

    first_task

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LV6_MZ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
